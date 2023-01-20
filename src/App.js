3adaf1cb75b97c11fe8a1e2f00dedf17acf5a28c
Maxim <kosmos-frontend@bk.ru>
2023-01-20 16:51:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./Components/Header/Header";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { HomePage } from "./Components/HomePage/HomePage";
+
 
 function App() {
+  const [isDarkTheme, setDarkTheme] = useState(true);
+  const Theme = isDarkTheme ? "Dark" : "Light";
+  useEffect(() => {
+    document.body.classList.add(Theme);
+    return () => {
+      document.body.classList.remove(Theme);
+    };
+  }, [Theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header setDarkTheme={setDarkTheme} isDarkTheme={isDarkTheme} />
+      <HomePage isDarkTheme={isDarkTheme} />
     </div>
   );
 }
+
+
+
 
 export default App;
